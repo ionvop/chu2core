4e4ad5ac9969e8a4286c1f538f79bd4b84d21c7a
ionvop <ionvop@gmail.com>
2026-09-13 22:16:50 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-// React95 global styles + theme (Win95 base, overridden by our pastel theme)
-import "@react95/core/GlobalStyle";
-import "@react95/core/themes/win95.css";
-
 import "./index.css";
 
 import App from "./App";
