ab84aa0d66f583afb8b8f52dbaac54010251daaa
ionvop <ionvop@gmail.com>
2026-09-13 23:46:32 +0800

    Create schema.sql

---
+++ public/api/tools/schema.sql
@@ -0,0 +1,2 @@
+CREATE TABLE `sessions` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `key` TEXT UNIQUE, `created_at` TEXT DEFAULT (datetime()));
+CREATE TABLE "messages" (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `session_id` INTEGER REFERENCES `sessions`(`id`), `role` TEXT, `content` TEXT, `created_at` TEXT DEFAULT (datetime()));
