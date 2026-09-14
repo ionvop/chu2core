a11c8a134de2517b9a48d63a6dc454f430df20fd
ionvop <ionvop@gmail.com>
2026-09-14 11:51:50 +0800

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -21,13 +21,15 @@ export function Contact() {
           src={GIFS[0].src}
           alt={GIFS[0].alt}
           width={96}
-          className="pointer-events-none absolute -left-4 top-2 z-10 hidden animate-bob -rotate-6 sm:block"
+          draggable
+          className="absolute -left-4 top-2 z-10 hidden animate-bob -rotate-6 sm:block"
         />
         <Gif
           src={GIFS[1].src}
           alt={GIFS[1].alt}
           width={96}
-          className="pointer-events-none absolute -right-4 top-2 z-10 hidden animate-float-slow rotate-6 sm:block"
+          draggable
+          className="absolute -right-4 top-2 z-10 hidden animate-float-slow rotate-6 sm:block"
         />
         <Stamps count={3} width={80} className="-mb-2" />
 
@@ -45,13 +47,15 @@ export function Contact() {
           src={GIFS[2].src}
           alt={GIFS[2].alt}
           width={88}
-          className="pointer-events-none absolute -left-3 top-6 z-10 hidden animate-bob -rotate-6 sm:block"
+          draggable
+          className="absolute -left-3 top-6 z-10 hidden animate-bob -rotate-6 sm:block"
         />
         <Gif
           src={GIFS[3].src}
           alt={GIFS[3].alt}
           width={88}
-          className="pointer-events-none absolute -right-3 top-6 z-10 hidden animate-float-slow rotate-6 sm:block"
+          draggable
+          className="absolute -right-3 top-6 z-10 hidden animate-float-slow rotate-6 sm:block"
         />
         <WinCard
           title="socials.exe"
@@ -113,13 +117,15 @@ export function Contact() {
           src={GIFS[4].src}
           alt={GIFS[4].alt}
           width={88}
-          className="pointer-events-none absolute -left-3 top-6 z-10 hidden animate-bob -rotate-6 sm:block"
+          draggable
+          className="absolute -left-3 top-6 z-10 hidden animate-bob -rotate-6 sm:block"
         />
         <Gif
           src={GIFS[5].src}
           alt={GIFS[5].alt}
           width={88}
-          className="pointer-events-none absolute -right-3 top-6 z-10 hidden animate-float-slow rotate-6 sm:block"
+          draggable
+          className="absolute -right-3 top-6 z-10 hidden animate-float-slow rotate-6 sm:block"
         />
         <WinCard
           title="contact.exe"
@@ -146,7 +152,8 @@ export function Contact() {
           src={GIFS[6].src}
           alt={GIFS[6].alt}
           width={88}
-          className="pointer-events-none absolute -right-2 -top-4 z-10 hidden animate-bob rotate-3 sm:block"
+          draggable
+          className="absolute -right-2 -top-4 z-10 hidden animate-bob rotate-3 sm:block"
         />
         <Stamps count={4} width={72} className="-rotate-1" />
       </section>
