829869b4e0d2ae8a6e6ec413341a0ded54a8fa10
ionvop <ionvop@gmail.com>
2026-09-14 12:41:12 +0800

    Update schema.sql

--- public/api/tools/schema.sql
+++ public/api/tools/schema.sql
@@ -1,2 +1,3 @@
 CREATE TABLE `sessions` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `key` TEXT UNIQUE, `created_at` TEXT DEFAULT (datetime()));
 CREATE TABLE "messages" (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `session_id` INTEGER REFERENCES `sessions`(`id`), `role` TEXT, `content` TEXT, `created_at` TEXT DEFAULT (datetime()));
+CREATE TABLE "textboard" (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `content` TEXT, `updated_at` TEXT DEFAULT (datetime()));
