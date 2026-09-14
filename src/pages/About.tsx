02a63b17ffdd1585cceb8b280063ee845197f5c9
ionvop <ionvop@gmail.com>
2026-09-14 11:51:48 +0800

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -23,13 +23,15 @@ export function About() {
           src={GIFS[0].src}
           alt={GIFS[0].alt}
           width={96}
-          className="pointer-events-none absolute -left-4 top-2 z-10 hidden animate-bob -rotate-6 sm:block"
+          draggable
+          className="absolute -left-4 top-2 z-10 hidden animate-bob -rotate-6 sm:block"
         />
         <Gif
           src={GIFS[1].src}
           alt={GIFS[1].alt}
           width={96}
-          className="pointer-events-none absolute -right-4 top-2 z-10 hidden animate-float-slow rotate-6 sm:block"
+          draggable
+          className="absolute -right-4 top-2 z-10 hidden animate-float-slow rotate-6 sm:block"
         />
         <Stamps count={3} width={80} className="-mb-2" />
 
@@ -47,13 +49,15 @@ export function About() {
           src={GIFS[2].src}
           alt={GIFS[2].alt}
           width={88}
-          className="pointer-events-none absolute -left-3 top-6 z-10 hidden animate-bob -rotate-6 sm:block"
+          draggable
+          className="absolute -left-3 top-6 z-10 hidden animate-bob -rotate-6 sm:block"
         />
         <Gif
           src={GIFS[3].src}
           alt={GIFS[3].alt}
           width={88}
-          className="pointer-events-none absolute -right-3 top-6 z-10 hidden animate-float-slow rotate-6 sm:block"
+          draggable
+          className="absolute -right-3 top-6 z-10 hidden animate-float-slow rotate-6 sm:block"
         />
         <WinCard
           title="profile.exe"
@@ -136,13 +140,15 @@ export function About() {
           src={GIFS[4].src}
           alt={GIFS[4].alt}
           width={88}
-          className="pointer-events-none absolute -left-3 top-6 z-10 hidden animate-bob -rotate-6 sm:block"
+          draggable
+          className="absolute -left-3 top-6 z-10 hidden animate-bob -rotate-6 sm:block"
         />
         <Gif
           src={GIFS[5].src}
           alt={GIFS[5].alt}
           width={88}
-          className="pointer-events-none absolute -right-3 top-6 z-10 hidden animate-float-slow rotate-6 sm:block"
+          draggable
+          className="absolute -right-3 top-6 z-10 hidden animate-float-slow rotate-6 sm:block"
         />
         <WinCard
           title="waifu.exe"
@@ -167,7 +173,8 @@ export function About() {
           src={GIFS[6].src}
           alt={GIFS[6].alt}
           width={88}
-          className="pointer-events-none absolute -right-2 -top-4 z-10 hidden animate-bob rotate-3 sm:block"
+          draggable
+          className="absolute -right-2 -top-4 z-10 hidden animate-bob rotate-3 sm:block"
         />
         <Stamps count={4} width={72} className="-rotate-1" />
       </section>
